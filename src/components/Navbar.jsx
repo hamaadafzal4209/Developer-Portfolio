40145667724be06f52f025a8239372ec6e04adf9
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-25 04:24:55 -0700

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+function Navbar() {
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        {/* logo */}
+        <div className="text-lg font-semibold">Portfolio</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
